b7134e59863c19c910e6bdf5149e3142d1253343
Emmanuel Nyamekye Nti <39943217+zezor@users.noreply.github.com>
2025-08-06 14:17:17 +0000

    registering models at admin

--- pfmapp/pfm/admin.py
+++ pfmapp/pfm/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
+from .models import Budget, Expense, Income
+
 # Register your models here.
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ( 'user', 'name', 'amount', 'date', 'category' )
+    list_filter = ('user', 'date', 'category')
+    search_fields = ('name', 'category', 'user__username')
+
+@admin.register(Income)
+class IncomeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'amount', 'date', 'source')
+    list_filter = ('user', 'date', 'source')
+    search_fields = ('name', 'source', 'user__username')
+
+
+@admin.register(Budget)
+class BudgetAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'month', 'start_date', 'end_date')
+    list_filter = ('user', 'month')
+    search_fields = ('name', 'user__username')
