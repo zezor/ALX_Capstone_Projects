dd17d90061c0a268c3824ece14a5e4aa226e38a9
Emmanuel Nyamekye Nti <39943217+zezor@users.noreply.github.com>
2025-08-06 11:16:52 +0000

    creating custom user

--- pfmapp/pfmapp/settings.py
+++ pfmapp/pfmapp/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pfm',  # Your application
+    'accounts',  # Your custom user application
 ]
 
 MIDDLEWARE = [
