8acf1a95516cff8517fad9ca67808fbec3062d44
Emmanuel Nyamekye Nti <39943217+zezor@users.noreply.github.com>
2025-08-07 16:22:48 +0000

    n templates and update URL paths for login and logout

--- pfmapp/pfmapp/views.py
+++ pfmapp/pfmapp/views.py
@@ -1,7 +1,9 @@
 
 
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def home(request):
     if not request.user.is_authenticated:
         return redirect('login')
