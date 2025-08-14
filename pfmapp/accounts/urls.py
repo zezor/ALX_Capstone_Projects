8a9253089023b99e0acd124f076654004d42ae5b
Emmanuel Nyamekye Nti <39943217+zezor@users.noreply.github.com>
2025-08-14 15:30:03 +0000

    working on customer user model

--- pfmapp/accounts/urls.py
+++ pfmapp/accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from  . import views
 
 urlpatterns = [
-    path("register/", views.UserRegistrationView.as_view(), name ="user_registration")
+    path('register/', views.UserRegistrationView.as_view(), name='register')
 
 ]
