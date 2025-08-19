03f8ccc431da730893f3a1090319a99019cfcdd3
Emmanuel Nyamekye Nti <39943217+zezor@users.noreply.github.com>
2025-08-19 16:47:21 +0000

    commit

--- pfmapp/pfmapp/settings.py
+++ pfmapp/pfmapp/settings.py
@@ -127,3 +127,4 @@ LOGIN_URL = '/accounts/login/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.User'
+
--- pfmapp/pfmapp/views.py
+++ pfmapp/pfmapp/views.py
@@ -3,7 +3,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
-@login_required
+
 def home(request):
     if not request.user.is_authenticated:
         return redirect('login')
